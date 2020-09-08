946ba57efe3f40e382749569459ce2608b84cb3f
yael noyzath <yael9742402@gmail.com>
2020-09-08 21:43:57 +0300

    create components
    part of components

---
+++ sahlavim-client/src/app/components/Operators/operator-schedule/operator-schedule.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-operator-schedule',
+  templateUrl: './operator-schedule.component.html',
+  styleUrls: ['./operator-schedule.component.css']
+})
+export class OperatorScheduleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
