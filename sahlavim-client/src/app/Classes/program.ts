c1be5df548b5620095d590899e322da26fce828b
Yael <10yaeli10@gmail.com>
2020-09-10 12:12:56 +0300

    Create Part Of Program Fields

--- sahlavim-client/src/app/Classes/program.ts
+++ sahlavim-client/src/app/Classes/program.ts
@@ -1,2 +1,27 @@
+import { Time } from '@angular/common';
+
 export class Program {
+    constructor(public ProgramId:number,
+        public ProgramType:number,
+        public ProgramName:string,
+        public BudgetItem:string,//סעיף תקציב
+        public FromDate:Date,
+        public ToDate:Date,
+        public NumMorningActivities:number,//מיספר הפעלות בוקר
+        public NumAfternoonActivities:number,//מיספר הפעלות צהרים
+        public NumPreferenceActivities:number,//מספר הפעלות מועדפות
+        public NumPreferenceActivitiesInWeek:number,//מספר הפעלות מועדפות לשבוע
+        public StartTimeMorning:Time,
+        public ToTimeMorning:Time,
+        public StartTimeAfternoon:Time,
+        public ToTimeAfternoon:Time,
+        public areTwoActivitiesInDay:boolean,
+        public StatusType:number,
+        public CreateByUserId:number,
+        public CreateDate:Date,
+        public 
+
+
+    ){}
+  
 }
