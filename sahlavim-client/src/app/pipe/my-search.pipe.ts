18a86d19ab3d159259e542356b03bd3a096fd66f
yael noyzath <yael9742402@gmail.com>
2020-10-12 23:04:09 +0300

    Small additions

--- sahlavim-client/src/app/pipe/my-search.pipe.ts
+++ sahlavim-client/src/app/pipe/my-search.pipe.ts
@@ -7,10 +7,14 @@ export class MySearchPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     return value.filter(x => 
-       x.nvOperatorName.indexOf(args) != -1 || x.nvContactPerson.indexOf(args) != -1
-    || x.nvOperatorTypeValue.indexOf(args) != -1 || x.nvCompanyName.indexOf(args) != -1
-    || x.nvActivityies.indexOf(args) != -1 || x.nvIdentity.indexOf(args) != -1 
-    || x.nvContactPersonPhone.indexOf(args) != -1 || x.nvContactPersonMail.indexOf(args) != -1
+       x.nvOperatorName.indexOf(args) != -1 
+    || x.nvContactPerson.indexOf(args) != -1
+    || x.nvOperatorTypeValue.indexOf(args) != -1
+    || x.nvCompanyName.indexOf(args) != -1
+    || x.nvActivityies.indexOf(args) != -1
+    || x.nvIdentity.indexOf(args) != -1 
+    || x.nvContactPersonPhone.indexOf(args) != -1
+    || x.nvContactPersonMail.indexOf(args) != -1
     || x.bInProgramPool.indexOf(args) != -1);
   }
 }
