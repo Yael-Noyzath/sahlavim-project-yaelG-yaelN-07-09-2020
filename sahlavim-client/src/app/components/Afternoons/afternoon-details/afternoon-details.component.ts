b508a1aec99ece317308638f5b5428c56e64c1da
yael noyzath <yael9742402@gmail.com>
2020-10-15 16:06:26 +0300

    CREATE PROJECT

---
+++ sahlavim-client/src/app/components/Afternoons/afternoon-details/afternoon-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-afternoon-details',
+  templateUrl: './afternoon-details.component.html',
+  styleUrls: ['./afternoon-details.component.css']
+})
+export class AfternoonDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
