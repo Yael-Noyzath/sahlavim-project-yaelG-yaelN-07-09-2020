a5c8e4210cb1a97bf2eb39fafa05d0cc8457d578
yael noyzath <yael9742402@gmail.com>
2020-10-18 16:11:57 +0300

    add files for my project

---
+++ sahlavim-client/src/app/components/Afternoons/afternoon-main/afternoon-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-afternoon-main',
+  templateUrl: './afternoon-main.component.html',
+  styleUrls: ['./afternoon-main.component.css']
+})
+export class AfternoonMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
