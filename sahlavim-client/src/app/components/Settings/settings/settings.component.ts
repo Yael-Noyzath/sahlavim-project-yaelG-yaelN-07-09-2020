a96ac54116adce5935089d4e2009776b92eaf6a9
yael noyzath <yael9742402@gmail.com>
2020-10-18 17:43:02 +0300

    setting table

--- sahlavim-client/src/app/components/Settings/settings/settings.component.ts
+++ sahlavim-client/src/app/components/Settings/settings/settings.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/classes/user';
+import { MainServiceService } from 'src/app/services/MainService/main-service.service';
+import { AfterViewInit, ViewChild, Component, OnInit } from '@angular/core';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { Operator } from 'src/app/Classes/operator';
+import { MySearchPipe } from 'src/app/pipe/my-search.pipe';
+import { from } from 'rxjs';
+import { Setting } from 'src/app/Classes/setting';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +15,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
 
-  constructor() { }
+  displayedColumns: string[] = ['iSettingId', 'nvSettingName', 'nvSettingCode', 'nvSettingTypeValue', 'nvAddress', 'nvPhone',
+    'nvContactPerson', 'nvContactPersonMail', 'nvContactPersonPhone', 'lSettingAgegroupsValue', 'nvFullName', 'nvMail', 'nvPhoneCoordinator'];
 
-  ngOnInit() {
+  //סוג מקור הנתונים
+  dataSource: MatTableDataSource<Setting>;
+  //מערך מפעילים לטבלה
+  settingList: Array<Setting>;
+
+  currentUser: User = new User();
+  constructor(private mainService: MainServiceService) {
+    this.currentUser = mainService.getUser();
+    this.SettingsGet();
   }
 
+  ngOnInit() {
+    this.ngAfterViewInit();
+  }
+
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  SettingsGet() {
+    this.mainService.post("SettingsGet", {}).then(
+      res => {
+        this.settingList = res;
+        this.dataSource = new MatTableDataSource(this.settingList);
+
+      },
+      err => {
+        alert("SettingsGet err")
+      }
+    );
+  }
 }
