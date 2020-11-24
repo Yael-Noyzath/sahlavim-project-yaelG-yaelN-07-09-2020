28de72662cdd25b08c2e2bcc40cf9976e54fbf15
yael noyzath <yael9742402@gmail.com>
2020-11-24 20:52:32 +0200

    Revert "afternoon and program date and time"
    This reverts commit 2126b3591c9461b7812f6cafb4dccd2d32e4215a.

--- sahlavim-client/src/app/services/MainService/main-service.service.ts
+++ sahlavim-client/src/app/services/MainService/main-service.service.ts
@@ -40,7 +40,7 @@ export class MainServiceService {
   operatorsList: Operator[] = [];
   settingsList: Setting[] = [];
   programsList: Program[] = [];
-  afternoonsList: Program[] = [];
+  afternoonsList:Program[]=[];
   //משתמש שנכנס למערכת
   currentUser: User = new User();
   // לעריכת מפעיל
@@ -57,8 +57,8 @@ export class MainServiceService {
 
   // sahlavimUrl = "http://localhost:53070/Service1.svc/";//שרת מקומי
   sahlavimUrl = "http://qa.webit-track.com/SachlavimQA/Service/Service1.svc/";
-
-
+ 
+ 
   post(url: string, data: any): Promise<any> {
     console.log(url);
     return this.http.post(`${this.sahlavimUrl}${url}`, data).toPromise();
@@ -71,16 +71,10 @@ export class MainServiceService {
 
   getPrograms() {
     //פונקציה המחזירה לתוך אובייקט את נתוני טבלת SysTable
-    this.post("ProgramsGet", { bProgramAfternoon: false }).then(
+    this.post("ProgramsGet", {}).then(
       res => {
-        if (res) {
+        if (res)
           this.programsList = res;
-          for (let p of this.programsList) {
-            p.dFromDate = new Date(parseInt(p.dFromDate.replace(/\/+Date\(([\d+-]+)\)\/+/, '$1'))).toJSON().slice(0,10);
-            p.dToDate = new Date(parseInt(p.dToDate.replace(/\/+Date\(([\d+-]+)\)\/+/, '$1'))).toJSON().slice(0,10);
-            // p.tFromTimeAfternoon=new Date(parseInt(p.tFromTimeAfternoon.replace(/\/+Date\(([\d+-]+)\)\/+/, '$1'))).toDateString();
-          }
-        }
       },
       err => {
         alert("ProgramsGet err")
@@ -88,21 +82,11 @@ export class MainServiceService {
     );
   }
   getAfternoon() {
-    this.post("ProgramsGet", { bProgramAfternoon: true }).then(
+    this.post("ProgramsGet", {bProgramAfternoon:true}).then(
       res => {
-        if (res) {
+        if (res)
           this.afternoonsList = res;
-          for (let p of this.afternoonsList) {
-            p.dFromDate = new Date(parseInt(p.dFromDate.replace(/\/+Date\(([\d+-]+)\)\/+/, '$1'))).toJSON().slice(0,10);
-            p.dToDate =   new Date(parseInt(  p.dToDate.replace(/\/+Date\(([\d+-]+)\)\/+/, '$1'))).toJSON().slice(0,10);
-            //  alert(p.dToDate[1])
-            //   if (this.YearTypeValue.get(p.iYearType) != p.dToDate[3]) {
-
-            //   }
-            // p.tFromTimeAfternoon=new Date(parseInt(p.tFromTimeAfternoon.replace(/\/+Date\(([\d+-]+)\)\/+/, '$1'))).toDateString();
-          }
-
-        }
+          debugger;
       },
       err => {
         alert("getAfternoon err")
