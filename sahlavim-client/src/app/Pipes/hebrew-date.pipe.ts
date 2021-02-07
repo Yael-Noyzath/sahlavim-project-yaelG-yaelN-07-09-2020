152377cb192df1f486f5827fe84595cc739db658
Yael <10yaeli10@gmail.com>
2021-02-07 14:59:26 +0200

    calendar

--- sahlavim-client/src/app/Pipes/hebrew-date.pipe.ts
+++ sahlavim-client/src/app/Pipes/hebrew-date.pipe.ts
@@ -10,6 +10,19 @@ export class HebrewDatePipe implements PipeTransform {
   transform(date: Date): any {
 
     var day = new this.Hebcal.HDate(date);
+    console.log(day.getSedra('h'));
+    console.log(day)
+    debugger
+    if(day.holidays(true).length>0)
+    {
+     console.log(day.holidays(true)[0].getDesc('h'))
+    }
+    if(day.getDay()==6)
+    {
+      console.log(day.getSedra('h')[0])
+      return this.Hebcal.gematriya(day.getDate())+' '+day.getSedra('h')[0];
+
+    }
     return this.Hebcal.gematriya(day.getDate());
     }
 
